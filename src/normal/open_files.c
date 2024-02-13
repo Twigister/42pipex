f946ea458fb02b6aed5e546538c65556f0f9838a
Armand Larzilliere <arlarzil@bess-f1r5s11.clusters.42paris.fr>
2024-02-13 14:55:26 +0100

    Fixed err message when command is preceded by a space

--- src/normal/open_files.c
+++ src/normal/open_files.c
@@ -39,8 +39,8 @@ static void	write_prog_name(const char *s)
 	int	len;
 
 	len = 0;
-	while (*s && *s > 32)
-		++len;
+	while (*s && *s <= 32)
+		++s;
 	while (s[len] > 32)
 		++len;
 	write(2, s, len);
